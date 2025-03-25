6219bc1ae900ab8d6a228402fe6a142ea0d4314b
shreyathal <thalvayapati.s@northeastern.edu>
2025-03-25 10:50:06 -0400

    first build

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/city-explorer-example/', 
   plugins: [svelte()],
+  build: {
+    outDir: 'docs', 
+    emptyOutDir: true
+  }
 })
